a0a8e4c450532d750cdc89ef64c084994517337a
Luciano_progrmador <castellanosluciano18@gmail.com>
2025-07-08 15:09:23 -0300

    nav Terminado

--- src/App.vue
+++ src/App.vue
@@ -33,12 +33,12 @@ export default {
 }
 
 #app {
+  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 h1{
   background-color: var(--color-primario);
