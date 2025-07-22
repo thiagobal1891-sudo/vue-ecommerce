ee67f0167b293a20f4f97684a57950e89815f294
thiago baltrons <thiagobalrami17@gmail.com>
2025-07-22 20:15:32 -0300

    agrego cambios en banner.vue

--- src/components/BannerFooter.vue
+++ src/components/BannerFooter.vue
@@ -7,16 +7,18 @@
     </div>
 
 
+
       <div class="card skincare">
         <h3>Skincare<br>Essentials</h3>
         <div class="arrow">→</div>
-    </div>
+      </div>
+
       <div class="card facepacks">
         <h4>Facepacks<br>& Peels</h4>
         <div class="arrow">→</div>
       </div>
-    </div>
 
+  </div>
 
 </template>
 
@@ -36,16 +38,24 @@ export default {
 </script>
 
 <style>
+    * {
+      box-sizing: border-box;
+    }
 
+    body {
+      margin: 0;
+      font-family: 'Arial', sans-serif;
+      padding: 20px;
+    }
 
     .banner-grid {
       display: grid;
-      grid-template: 
-                    "lifeStyle lifeStyle" 400px
+          grid-template:
+                   "lifeStyle lifeStyle" 400px
 
-                    "skincare    facepacks" 228px
+                   "skincare   facepacks"  228px
 
-                    /1fr    1fr   ;
+                   /1fr    1fr    ;
       gap: 20px;
       max-width: 1200px;
       margin: 0 auto;
@@ -63,20 +73,13 @@ export default {
       grid-area: lifeStyle;
     }
 
-    .lifestyle-card:hover {
-    transform: scale(1.05);
-    transition: 0.5s;
-}
-
-    .lifestyle-card:hover:before{
-    position: absolute;
-    top: 150px;
-}
-
     .lifestyle-card h2 {
   width: 154px;
   height: 20px; 
+  left: 59px;
+  top: 1999px;
   font-family: 'Inter', sans-serif;
+  font-style: normal;
   font-weight: 400;
   font-size: 30px;
   line-height: 20px;
@@ -90,7 +93,10 @@ export default {
     .lifestyle-card h1 {
   width: 618px;
   height: 136px;
+  left: 59px;
+  top: 2038px;
   font-family: 'Inter', sans-serif;
+  font-style: normal;
   font-weight: 700;
   font-size: 52px;
   line-height: 68px;
@@ -100,6 +106,7 @@ export default {
 
 
     }
+
 
     .card {
       border-radius: 20px;
@@ -114,26 +121,16 @@ export default {
       justify-content: flex-start;
     }
 
-    .card:hover {
-    transform: scale(1.1);
-    transition: 0.5s;
-}
-
-.card:hover::before{
-    position: absolute;
-    top: 150px;
-}
-
     .card h3 {
-  width: 232px;
+  width: 309px;
   height: 104px;
   font-family: 'Inter', sans-serif;
   font-weight: 700;
   font-size: 40px;
   text-align: right;
   color: #A53F64;
-  position: absolute;
-  right: 30px;
+        position: absolute;
+      right: 30px;
     }
 
 .card h4 {
@@ -144,8 +141,8 @@ export default {
   font-size: 40px;
   text-align: right;
   color: #1B4B66;
-  position: absolute;
-  right: 30px;
+        position: absolute;
+      right: 30px;
 }
 
     .card .arrow {
@@ -159,22 +156,15 @@ export default {
       color: #000;
       z-index: 2;
     }
-    .card .arrow:hover {
-    transform: scale(1.2);
-    transition: 0.5s;
-}
-    .card .arrow:hover:before{
-    position: absolute;
-    top: 150px;
-}
 
-  .skincare {
+    .skincare {
       background-image: url('https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1000w,f_avif,q_auto:eco,dpr_2/rockcms/2024-01/240117-staff-skin-care-routines-social-2c85d8.jpg');
       grid-area: skincare;
     }
-  .facepacks {
+
+    .facepacks {
       background-image: url('https://shipcartecommerce.com/cdn/shop/collections/877f03da31ad8bb130dde99698f7d3ec.jpg?v=1748635689');
-    grid-area: facepacks;
+      grid-area: facepacks;
     }
 
     @media (max-width: 768px) {
@@ -185,7 +175,10 @@ export default {
       .card h3 {
         font-size: 20px;
       }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 5c22470f21a49fcb2b18586b12ce673f70efc4fa
       .card h4 {
         font-size: 20px;
       }
