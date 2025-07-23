08a117b70320980e3be25b15d24a2b6fed272725
unknown <plopez@ibec.edu.uy>
2025-07-22 21:42:30 -0300

    adapto parte a mobile

--- src/components/BannerVue.vue
+++ src/components/BannerVue.vue
@@ -8,7 +8,8 @@
     :navigation = "false"
   >
     <swiper-slide v-for="card in cards" :key="card.id">
-        <div class="card__image" :style="{ backgroundImage: 'url(' + card.image + ')' }">
+        <div class="card__image">
+            <img :src="card.image" alt=""  class="card__image__image">
             <div class="card__image__content">
                 <div class="card__image__content__title">
                     {{card.title}} 
@@ -17,9 +18,9 @@
                     {{card.text }}
                 </div>
                
-               <div class="card__image__content__button">
-                <button> <p class="arrow_icon">➜</p>  <p>See more </p></button>
-               </div>
+               <button class="card__image__content__button">
+                 <p class="arrow_icon">➜</p>  <p>See more </p>
+               </button>
             </div>
         </div>
     </swiper-slide>
@@ -47,13 +48,13 @@ export default {
             id:0,
             title:"Carry your Funk",
             text:"Trendy handbags collection for your party animal",
-            image:"https://i.pinimg.com/736x/01/c4/00/01c40028627c7ce90fb1f7d94ba1856c.jpg"
+            image:"https://img.hogar.mapfre.es/wp-content/uploads/2018/08/barnizar_una_pared_de_ladrillo.jpg"
         },
         {
             id:1,
             title:"Carry your Funk",
             text:"Trendy handbags collection for your party animal",
-            image: "https://i.pinimg.com/736x/01/c4/00/01c40028627c7ce90fb1f7d94ba1856c.jpg"
+            image: "https://img.hogar.mapfre.es/wp-content/uploads/2018/08/barnizar_una_pared_de_ladrillo.jpg"
         }
       ]
     }
@@ -62,66 +63,59 @@ export default {
 </script>
 
 <style>
-
-.swiper {
+.card__image {
+  display: flex;
+  justify-content: flex-end;
   width: 100%;
-  height: 200px;
+  position: relative;
+  font-size: 1vw;
 }
-
+.card__image__image{
+  width: 100%;
+}
 .card__image__content{
-width: 759px;
-height: 316px;
-padding-left: 40px;
-opacity: 1;
-top: 196px;
-left: 563px;
-border-radius: 24px;
-
-background: rgba(222, 222, 222, 0.7);
-backdrop-filter: blur(5px)
+  width: 100%;
+  height: 70%;
+  padding-left: 40px;
+  opacity: 1;
+  top: 15%;
+  left: 50%;
+  border-radius: 24px;
+  background: rgba(222, 222, 222, 0.7);
+  backdrop-filter: blur(5px);
+  position:absolute;
 } 
 .card__image__content__title{
-display: flex;
-width: 853px;
-height: 84px;
-opacity: 1;
-color: rgba(27, 75, 102, 1);
-
-font-family: Inter;
-font-weight: 800;
-font-size: 50px;
-
-line-height: 84px;
-letter-spacing: 0%;
-vertical-align: middle;
-
+  display: flex;
+  height: 50px;
+  opacity: 1;
+  color: rgba(27, 75, 102, 1);
+  font-family: Inter;
+  font-weight: 800;
+  font-size: 2em;
+  letter-spacing: 0%;
 }
 
 .card__image__content__text{
-display: flex;
-text-align: left;
-width: 530px;
-height: 50px;
-opacity: 1;
-
-font-family: Inter;
-font-weight: 500;
-font-size: 20px;
-line-height: 38px;
-letter-spacing: 0%;
-vertical-align: middle;
-
-
-color: #1B4B66;
-
-
+  display: flex;
+  text-align: left;
+  width: 530px;
+  height: 50px;
+  opacity: 1;
+  font-family: Inter;
+  font-weight: 500;
+  font-size: 1em;
+  line-height: 38px;
+  letter-spacing: 0%;
+  vertical-align: middle;
+  color: #1B4B66;
 }
-.card__image__content__button button {
+.card__image__content__button {
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 180px;
-  height: 44px;
+  width: 20%;
+  height: 12%;
   flex-direction: row;
   padding: 10px 20px;
   background-color: #1B4B66;
@@ -130,24 +124,14 @@ color: #1B4B66;
   border-radius: 4px;
   cursor: pointer;
   gap: 10px;
+  font-size: 1vw;
 }
 
 .card__image__content__button p {
   margin: 0;
+  font-size: 1em;
 }
-.card__image__content img {
-  width: 1436px;
-  height: 514px;
-  background-repeat: no-repeat;      
-  background-position: center;       
-  background-size: contain;  
-  
-}
-.card__image {
-  display: flex;
-  justify-content: flex-end;
 
-}
 .arrow_icon {
   width: 20px;
   font-size: 1.5rem;
